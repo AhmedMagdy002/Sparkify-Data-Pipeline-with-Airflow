e9b33060f4f4388d47519e25289828ac234f796e
Ahmed Eltohamy <136391413+AhmedMagdy002@users.noreply.github.com>
2025-06-10 00:31:22 +0300

    Create example_dag.py

---
+++ dags/example_dag.py
@@ -0,0 +1,123 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+from airflow.models import Variable
+
+# Get configuration from Airflow Variables
+s3_bucket = Variable.get("s3_bucket")
+s3_log_key = Variable.get("s3_log_key")
+s3_song_key = Variable.get("s3_song_key_demo") 
+s3_log_json_path = Variable.get("s3_log_json_path")
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': datetime(2025, 6, 1),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *',
+          max_active_runs=1
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_events",
+    s3_bucket=s3_bucket,
+    s3_key=s3_log_key,
+    region="us-west-2",
+    json_path=s3_log_json_path
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_songs",
+    s3_bucket=s3_bucket,
+    s3_key=s3_song_key,  # This will use 'song-data/A/A/A' from your variables
+    region="us-west-2",
+    json_path="auto"
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songplays",
+    sql_query=SqlQueries.songplay_table_insert
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    sql_query=SqlQueries.user_table_insert,
+    truncate_table=True
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songs",
+    sql_query=SqlQueries.song_table_insert,
+    truncate_table=True
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="artists",
+    sql_query=SqlQueries.artist_table_insert,
+    truncate_table=True
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="time",
+    sql_query=SqlQueries.time_table_insert,
+    truncate_table=True
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    redshift_conn_id="redshift",
+    tables=['songplays', 'users', 'songs', 'artists', 'time'],
+    dq_checks=[
+        {'check_sql': "SELECT COUNT(*) FROM users WHERE userid is null", 'expected_result': 0},
+        {'check_sql': "SELECT COUNT(*) FROM songs WHERE songid is null", 'expected_result': 0}
+    ]
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+# Define task dependencies
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+[stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, 
+                         load_artist_dimension_table, load_time_dimension_table]
+[load_user_dimension_table, load_song_dimension_table, 
+ load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
+run_quality_checks >> end_operator
